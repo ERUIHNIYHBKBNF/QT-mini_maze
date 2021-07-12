1a5750c2aad5d14c46a0a7c868c39f3fbc0aa0de
ERUIHNIYHBKBNF <69080784+ERUIHNIYHBKBNF@users.noreply.github.com>
2021-07-12 15:52:29 +0800

    新增迷宫地图类

---
+++ mazemap.h
@@ -0,0 +1,19 @@
+#ifndef MAZEMAP_H
+#define MAZEMAP_H
+#include <vector>
+using std::vector;
+
+class mazemap
+{
+public:
+    mazemap();
+    int getHeight();
+    int getWidth();
+    vector<vector<int> > getMap();
+    void setSize(int, int);
+    void setMap(vector<vector<int> >);
+private:
+    int height, width;
+    vector<vector<int> > map;
+};
+#endif // MAZEMAP_H
